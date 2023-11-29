076dee7f48c72e652c5523ab3a1d790e9d245a73
R4ID3NZ4P <iamxtremesifat@gmail.com>
2023-11-30 04:00:42 +0600

    Individual test info query API created

--- index.js
+++ index.js
@@ -10,6 +10,63 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sml8dnv.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    //await client.connect();
+
+    const userCollection = client.db("diagnostechDB").collection("users");
+    const testCollection = client.db("diagnostechDB").collection("tests");
+
+    //users collection related APIs
+    app.post("/users", async (req, res) => {
+        const userInfo = req.body;
+        const query = {email: userInfo.email};
+        const queryRes = await userCollection.findOne(query);
+        if(queryRes) return res.send({message: "User already exists", insertedId: null});
+        const result = await userCollection.insertOne(userInfo);
+        res.send(result);
+    });
+
+    app.get("/users/admin/:email", async (req, res) => {
+        const query = {email: req.params.email};
+        const result = await userCollection.findOne(query);
+        res.send(result);
+    });
+
+    // tests related APIs
+    app.get("/tests", async (req, res) => {
+        const result = await testCollection.find().toArray();
+        res.send(result);
+    });
+
+    app.get("/tests/:id", async (req, res) => {
+        const query = {_id: new ObjectId(req.params.id)};
+        const result = await testCollection.findOne(query);
+        res.send(result);
+    });
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
     res.send("Server status: Up");
